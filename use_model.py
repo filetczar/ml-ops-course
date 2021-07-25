2fd5a15678c95b89944d270a6ad729f9c00c2ac7
Phil Azar <azar.phil@gmail.com>
2021-07-25 17:06:21 -0500

    setting up for gcp

--- use_model.py
+++ use_model.py
@@ -1,13 +1,13 @@
 import pickle 
 import numpy as np 
+import json
+import requests 
 
-local_classifier = pickle.load(open('classifier.pickle', 'rb'))
-local_scaler = pickle.load(open('scaler.pickle', 'rb')) 
-
+url = "http://127.0.0.1:8005/model"
 new_obs = np.array([[40, 20000], [50,50000], [23, 15000]])
 
-new_preds = local_classifier.predict(local_scaler.transform(new_obs))
-new_probs = local_classifier.predict_proba(local_scaler.transform(new_obs)) 
+request_data = json.dumps({'age': 50, 'salary': 10000})
+response = requests.post(url, request_data)
+print(response.text)
 
-print(new_preds)
-print(new_probs)
+
